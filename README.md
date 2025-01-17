c6c634e8b8c36d1e1c06e0b9adead01f72581835
Mário Henrique <mariohcmv@gmail.com>
2025-01-17 00:29:42 -0300

    Atualizado

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Aula de consumo de API - 
+Aula de consumo de API - Projeto para criar página de vendas com função de adicionarmanipular produtos na API
+
--- script.js
+++ script.js
@@ -1,47 +1,64 @@
 // Função para abrir o formulário
 function abrirFormulario() {
-    document.getElementById("modalCadastroProduto").style.display = "block";
+  document.getElementById("modalCadastroProduto").style.display = "block";
+}
+
+// Função para fechar o formulário
+function fecharFormulario() {
+  document.getElementById("modalCadastroProduto").style.display = "none";
+}
+
+// Fechar o modal se o usuário clicar fora da caixa de conteúdo
+window.onclick = function (evento) {
+  if (evento.target == document.getElementById("modalCadastroProduto")) {
+    fecharFormulario();
   }
-  
-  // Função para fechar o formulário
-  function fecharFormulario() {
-    document.getElementById("modalCadastroProduto").style.display = "none";
-  }
-  
-  // Fechar o modal se o usuário clicar fora da caixa de conteúdo
-  window.onclick = function (evento) {
-    if (evento.target == document.getElementById("modalCadastroProduto")) {
-      fecharFormulario();
-    }
-  };
-  
+};
 
 
-  async function carregarProdutos(){
-    const url = 'https://675b512e9ce247eb1936449d.mockapi.io/Uvas'
-    const resposta = await fetch(url)
-    const dadosProdutos = await resposta.json()
 
-    const main = document.querySelector('#produtos-galeria')
-    main.innerHTML = ''
-    dadosProdutos.forEach(produto => {
-        const estruturaHtmlProduto = `
-        <section class="cartao-item">
-            <img src="${produto.imagem}" alt="Item 1" />
-            <h3>${produto.nome}</h3>
-            <p class="preco-item">R$ ${produto.preco}</p>
-            <button class="botao-comprar">Comprar</button>
-        </section>
-        `
-        main.innerHTML += estruturaHtmlProduto
-  })
+async function carregarProdutos(filtro=''){ // definir por padrão filtro vazio
+  const url = 'https://675b512e9ce247eb1936449d.mockapi.io/Uvas'
+  const resposta = await fetch(url)
+  const dadosProdutos = await resposta.json()
 
-  }
+  const produtosFiltrados = dadosProdutos.filter(produto => produto.nome.toLowerCase().includes(filtro.toLowerCase()))
 
-  async function adicionarProduto(){
+
+  const main = document.querySelector('#produtos-galeria')
+  main.innerHTML = ''
+  produtosFiltrados.forEach(produto => {
+      const estruturaHtmlProduto = `
+      <section class="cartao-item">z
+          <img src="${produto.imagem}" alt="Item 1" />
+          <h3>${produto.nome}</h3>
+          <p class="preco-item">R$ ${produto.preco}</p>
+          <button class="botao-comprar">Comprar</button>
+      </section>
+      `
+      main.innerHTML += estruturaHtmlProduto
+})
+
+}
+
+
+async function adicionarProduto(){
+  try{
     const nomeDigitado = document.querySelector('#nomeProduto').value
     const precoDigitado = document.querySelector('#precoProduto').value
     const urlImagem = document.querySelector('#imagemProduto').value
+
+    if(!nomeDigitado){  // opção de uso do OU = pipi -> if(!nomeDigitado || !precoDigitado || !imagemProduto
+      throw new Error("O nome não foi preenchido.") 
+    }
+    if(!precoDigitado){
+      throw new Error("O preço não foi digitado.")
+    }
+    if(!imagemProduto){
+      throw new Error("A imagem não foi adicionada.")
+    }
+
+
     const dadosProduto = {
         nome: nomeDigitado,
         preco: precoDigitado,
@@ -54,11 +71,23 @@ function abrirFormulario() {
         headers: {'Content-Type':'application/json'},
         body: JSON.stringify(dadosProduto)
     })
+
+    if(!resposta.ok){
+      throw new Error("erro ao add na api.", resposta.statusText)
+    }
+
     alert('Produto adicionado com sucesso')
     carregarProdutos()
-
-
+    fecharFormulario()
+  } catch(erro){
+    console.error("Erro ao cadastrar produto.", erro.message)
   }
 
-  carregarProdutos()
- 
+}
+
+const inputBarraPesquisar = document.querySelector('#pesquisar')
+inputBarraPesquisar.addEventListener("input",(evento) => {
+  const produtoDigitado = inputBarraPesquisar.value
+  carregarProdutos(produtoDigitado)
+})
+carregarProdutos()
